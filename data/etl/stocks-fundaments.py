0b6cfcf185010aee5c53fc9bb77ab6c005692af3
boa50 <bruno.albuquerque50@gmail.com>
2024-10-07 09:57:59 -0300

    Organised the code to retrieve the Patrimonial Value

--- data/etl/stocks-fundaments.py
+++ data/etl/stocks-fundaments.py
@@ -2,43 +2,68 @@ import pandas as pd
 import datetime
 import os.path
 
+
+def load_files(years_load, files_load):
+    df = pd.DataFrame()
+
+    for year in years_load:
+        for file in files_load:
+            fname = "data/raw/" + file + str(year) + ".csv"
+
+            if os.path.isfile(fname):
+                df = pd.concat(
+                    [
+                        df,
+                        pd.read_csv(
+                            fname,
+                            encoding="ISO-8859-1",
+                            sep=";",
+                        ),
+                    ]
+                )
+            else:
+                print(fname + " not found! Skipping")
+
+    return df
+
+
+def prepare_dataframe(df, cd_cvm_load):
+    df = df[df["CD_CVM"].isin(cd_cvm_load)]
+    df = df[df["ORDEM_EXERC"] == "ÚLTIMO"]
+    df = df[["CD_CVM", "DT_INI_EXERC", "DT_FIM_EXERC", "DS_CONTA", "VL_CONTA"]]
+    df["DT_FIM_EXERC"] = pd.to_datetime(df["DT_FIM_EXERC"])
+    df["DT_INI_EXERC"] = pd.to_datetime(df["DT_INI_EXERC"])
+    df["EXERC_YEAR"] = df["DT_FIM_EXERC"].dt.year
+
+    return df
+
+
+df_fundaments = pd.DataFrame(
+    columns=[
+        "CD_CVM",
+        "DT_INI_EXERC",
+        "DT_FIM_EXERC",
+        "DS_CONTA",
+        "VL_CONTA",
+        "EXERC_YEAR",
+        "VL_CONTA_ROLLING_YEAR",
+    ]
+)
+
 ### Only 2022 forward became mandatory to share info with CVM
 years_load = [2022, 2023, 2024]
-files_load = ["dfp_cia_aberta_DRE_con_", "itr_cia_aberta_DRE_con_"]
-
-df = pd.DataFrame()
-
-for year in years_load:
-    for file in files_load:
-        fname = "data/raw/" + file + str(year) + ".csv"
-
-        if os.path.isfile(fname):
-            df = pd.concat(
-                [
-                    df,
-                    pd.read_csv(
-                        fname,
-                        encoding="ISO-8859-1",
-                        sep=";",
-                    ),
-                ]
-            )
-        else:
-            print(fname + " not found! Skipping")
 
 ### Getting only companies available on basic info file
 df_basic_info = pd.read_csv("data/processed/stocks-basic-info.csv")
-df = df[df["CD_CVM"].isin(df_basic_info["CD_CVM"].values)]
-
-
-df = df[df["ORDEM_EXERC"] == "ÚLTIMO"]
-df = df[["CD_CVM", "DT_INI_EXERC", "DT_FIM_EXERC", "DS_CONTA", "VL_CONTA"]]
-df["DT_FIM_EXERC"] = pd.to_datetime(df["DT_FIM_EXERC"])
-df["DT_INI_EXERC"] = pd.to_datetime(df["DT_INI_EXERC"])
-df["EXERC_YEAR"] = df["DT_INI_EXERC"].dt.year
+cd_cvm_load = df_basic_info["CD_CVM"].values
 
 
 ### COMPANY PROFIT
+files_load = ["dfp_cia_aberta_DRE_con_", "itr_cia_aberta_DRE_con_"]
+
+df = load_files(years_load, files_load)
+df = prepare_dataframe(df, cd_cvm_load)
+
 profit_raw = df[df["DS_CONTA"] == "Lucro ou Prejuízo Líquido Consolidado do Período"]
 
 td_quarter = datetime.timedelta(days=93)
@@ -84,6 +109,37 @@ profit_quarter["VL_CONTA_ROLLING_YEAR"] = profit_quarter.groupby("CD_CVM")[
 
 profit_quarter["DS_CONTA"] = "Lucro ou Prejuízo"
 
+print()
+print("PROFIT")
 print(profit_quarter.head())
+print()
 
-profit_quarter.to_csv("data/processed/stocks-fundaments.csv", index=False)
+
+### PATRIMONIAL VALUE
+files_load = ["dfp_cia_aberta_BPP_ind_", "itr_cia_aberta_BPP_ind_"]
+
+df_pl = load_files(years_load, files_load)
+
+df_pl = df_pl[df_pl["DS_CONTA"] == "Patrimônio Líquido"]
+df_pl["DT_INI_EXERC"] = "1900-01-01"
+df_pl = prepare_dataframe(df_pl, cd_cvm_load)
+
+df_pl["VL_CONTA_ROLLING_YEAR"] = -1
+
+df_pl = df_pl.sort_values(by=["DT_FIM_EXERC", "CD_CVM"])
+
+print()
+print("PL")
+print(df_pl.head())
+print()
+
+
+### Consolidate final file
+df_fundaments = pd.concat([profit_quarter, df_pl])
+
+print()
+print("DF FUNDAMENTS")
+print(df_fundaments.head())
+print()
+
+df_fundaments.to_csv("data/processed/stocks-fundaments.csv", index=False)
