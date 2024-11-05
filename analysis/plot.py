2937ec13c140fe15a9c7cd92fefbbadec35d902c
boa50 <bruno.albuquerque50@gmail.com>
2024-11-05 15:25:01 -0300

    Made possible to plot different tickers concurrently

--- analysis/plot.py
+++ analysis/plot.py
@@ -9,7 +9,19 @@ _df_fundaments = pd.read_csv(
 )
 
 
-def _get_chart_props_fundaments(ticker, kpi):
+def _get_chart_props_fundaments_segmento(ticker, df_kpi, value_column):
+    segmento = info.get_segmento_by_ticker(ticker)
+    cd_cvm_segmento = info.get_companies_by_segmento(segmento=segmento)["CD_CVM"].values
+
+    df_segmento = df_kpi[df_kpi["CD_CVM"].isin(cd_cvm_segmento)]
+    df_segmento = df_segmento[["DATE", value_column]]
+    df_segmento = df_segmento.groupby("DATE").mean().reset_index()
+    df_segmento = df_segmento.sort_values(by="DATE")
+
+    return df_segmento
+
+
+def _get_chart_props_fundaments(ticker, kpi, is_segemento=True):
     cd_cvm = info.get_cd_cvm_by_ticker(ticker)
     first_date = _df_fundaments["DT_FIM_EXERC"].max() - pd.DateOffset(years=10)
 
@@ -25,24 +37,17 @@ def _get_chart_props_fundaments(ticker, kpi):
         else "VL_CONTA_ROLLING_YEAR"
     )
 
-    df_ticker = df_kpi[df_kpi["CD_CVM"] == cd_cvm]
-
-    segmento = info.get_segmento_by_ticker(ticker)
-    cd_cvm_segmento = info.get_companies_by_segmento(segmento=segmento)["CD_CVM"].values
-
-    df_segmento = df_kpi[df_kpi["CD_CVM"].isin(cd_cvm_segmento)]
-    df_segmento = df_segmento[["DATE", value_column]]
-    df_segmento = df_segmento.groupby("DATE").mean().reset_index()
-    df_segmento = df_segmento.sort_values(by="DATE")
+    if is_segemento:
+        df = _get_chart_props_fundaments_segmento(ticker, df_kpi, value_column)
+    else:
+        df = df_kpi[df_kpi["CD_CVM"] == cd_cvm]
 
     date_x_ticks = 10
 
-    return df_ticker, df_segmento, value_column, date_x_ticks
+    return df, value_column, date_x_ticks
 
 
-def _get_chart_props_history(ticker, kpi):
-    df_ticker = _df_history[_df_history["TICKER"] == ticker]
-
+def _get_chart_props_history_segmento(ticker, kpi):
     segmento = info.get_segmento_by_ticker(ticker)
     tickers_segmento = info.get_companies_by_segmento(segmento=segmento)[
         "MAIN_TICKER"
@@ -52,26 +57,35 @@ def _get_chart_props_history(ticker, kpi):
     df_segmento = df_segmento.groupby("DATE").mean().reset_index()
     df_segmento = df_segmento.sort_values(by="DATE")
 
-    value_column = kpi
+    return df_segmento
 
+
+def _get_chart_props_history(ticker, kpi, is_segemento=True):
+    if is_segemento:
+        df = _get_chart_props_history_segmento(ticker, kpi)
+    else:
+        df = _df_history[_df_history["TICKER"] == ticker]
+
+    value_column = kpi
     date_x_ticks = 100
 
-    return df_ticker, df_segmento, value_column, date_x_ticks
+    return df, value_column, date_x_ticks
 
 
-def plot_historical_kpi(ticker, kpi, show_segment=True):
-    if kpi in _df_fundaments["KPI"].unique():
-        df_ticker, df_segmento, value_column, date_x_ticks = (
-            _get_chart_props_fundaments(ticker, kpi)
-        )
-    else:
-        df_ticker, df_segmento, value_column, date_x_ticks = _get_chart_props_history(
-            ticker, kpi
-        )
-
+def plot_historical_kpi(tickers, kpi, show_segment=True):
     plt.figure(figsize=(10, 5))
+    is_df_fundaments = kpi in _df_fundaments["KPI"].unique()
 
     if show_segment:
+        if is_df_fundaments:
+            df_segmento, value_column, date_x_ticks = _get_chart_props_fundaments(
+                tickers[0], kpi, is_segemento=True
+            )
+        else:
+            df_segmento, value_column, date_x_ticks = _get_chart_props_history(
+                tickers[0], kpi, is_segemento=True
+            )
+
         plt.plot(
             df_segmento["DATE"],
             df_segmento[value_column],
@@ -80,18 +94,27 @@ def plot_historical_kpi(ticker, kpi, show_segment=True):
             color="grey",
         )
 
-    plt.plot(
-        df_ticker["DATE"],
-        df_ticker[value_column],
-        label=ticker,
-        linestyle="-",
-        color="blue",
-    )
+    for ticker in tickers:
+        if is_df_fundaments:
+            df, value_column, date_x_ticks = _get_chart_props_fundaments(
+                ticker, kpi, is_segemento=False
+            )
+        else:
+            df, value_column, date_x_ticks = _get_chart_props_history(
+                ticker, kpi, is_segemento=False
+            )
+
+        plt.plot(
+            df["DATE"],
+            df[value_column],
+            label=ticker,
+            linestyle="-",
+        )
 
     plt.title(kpi)
     plt.xlabel("Date")
     plt.ylabel(kpi)
-    plt.xticks(df_ticker["DATE"][::date_x_ticks], rotation=0)
+    plt.xticks(df["DATE"][::date_x_ticks], rotation=0)
     plt.legend()
 
     plt.tight_layout()
@@ -138,7 +161,7 @@ def plot_equity_evolution(ticker):
         color="steelblue",
     )
 
-    plt.title("Equity Evolution")
+    plt.title("Equity Evolution - " + ticker)
     plt.xlabel("Date")
     plt.ylabel("Value")
     plt.xticks(df_net_revenue["DATE"][::10], rotation=0)
