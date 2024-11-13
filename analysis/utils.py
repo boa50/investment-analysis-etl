10483d2db785cb4de35e0f066f5f9de0145ace76
boa50 <bruno.albuquerque50@gmail.com>
2024-11-13 15:40:15 -0300

    Created a function to calculate IPCA weights

--- analysis/utils.py
+++ analysis/utils.py
@@ -1,5 +1,24 @@
+import pandas as pd
+
+
+def get_ipca_weights(dates):
+    df_ipca = pd.read_csv("../data/processed/ipca.csv", parse_dates=["DATE"])
+
+    df_ipca["DATE"] = df_ipca["DATE"].dt.to_period("M")
+
+    kpi_periods = dates.dt.to_period("M").values
+
+    df_ipca = df_ipca[df_ipca["DATE"].isin(kpi_periods)]
+
+    last_ipca_value = df_ipca["VALUE"].iloc[-1]
+
+    weights = last_ipca_value / df_ipca["VALUE"]
+
+    return weights.reset_index(drop=True)
+
+
 def get_date_weights(dates):
     days_diff = (dates.max() - dates.min()).days
     weights = (days_diff - (dates.max() - dates).dt.days) / days_diff
 
-    return weights
+    return weights.reset_index(drop=True)
