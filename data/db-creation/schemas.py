914d7c0dbc405bc8a40794e805a198f76cfc531f
boa50 <bruno.albuquerque50@gmail.com>
2025-01-08 10:20:26 -0300

    Started creating more focused queries

--- data/db-creation/schemas.py
+++ data/db-creation/schemas.py
@@ -6,14 +6,14 @@ def get_schema(table_name):
                 bigquery.SchemaField("CD_CVM", "STRING", mode="REQUIRED"),
                 bigquery.SchemaField("DT_START", "DATE", mode="REQUIRED"),
                 bigquery.SchemaField("DT_END", "DATE", mode="REQUIRED"),
-                bigquery.SchemaField("DT_YEAR", "STRING", mode="REQUIRED"),
+                bigquery.SchemaField("DT_YEAR", "INTEGER", mode="REQUIRED"),
                 bigquery.SchemaField("KPI", "STRING", mode="REQUIRED"),
                 bigquery.SchemaField("VALUE", "BIGDECIMAL", mode="NULLABLE"),
                 bigquery.SchemaField("VALUE_ROLLING_YEAR", "BIGDECIMAL", mode="NULLABLE")
             ]
     elif table_name == "stocks-history":
         return [
-                bigquery.SchemaField("DT_EVENT", "DATE", mode="REQUIRED"),
+                bigquery.SchemaField("DATE", "DATE", mode="REQUIRED"),
                 bigquery.SchemaField("CD_CVM", "STRING", mode="REQUIRED"),
                 bigquery.SchemaField("TICKER", "STRING", mode="REQUIRED"),
                 bigquery.SchemaField("PRICE", "BIGDECIMAL", mode="NULLABLE"),
